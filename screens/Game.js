a084ca24a4d76ca7429e1189f47641620ac07483
Tiago José <tiago_jvo@outlook.com>
2020-06-02 22:27:05 -0300

    Adding game features hints & validation

--- screens/Game.js
+++ screens/Game.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View , Button } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -19,32 +19,54 @@ const generateRandomBetween = (min, max, exclude) => {
 const Game = (props) => {
   const [currentGuess, setCurrenGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
 
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === 'lower' && currentGuess < props.userChoice) ||
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert(`Don't lie!`, 'You know that this is wrong', [{ text: 'Sorry', style: 'cancel' }]);
+      return;
+    }
+
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+
+    const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+    setCurrenGuess(nextNumber)
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonsContainer}>
-          <Button title='LOWER' onPress={() => {}}></Button>
-          <Button title='GREATER' onPress={() => {}}></Button>
+        <Button title="LOWER" onPress={() => nextGuessHandler('lower')}></Button>
+        <Button title="GREATER" onPress={() => nextGuessHandler('greater')}></Button>
       </Card>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    screen : {
-        flex: 1,
-        alignItems: 'center',
-        padding: 10
-    },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+  },
 
-    buttonsContainer : {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginTop: 20,
-        width: 300,
-        maxWidth: '80%'
-    }
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%',
+  },
 });
 
 export default Game;
